dc7b900144a54317599bc7bfeed5731fe567b283
Blad3sy <oscarprosser@gmail.com>
2023-12-09 14:00:00 +0000

    Completed Day 9
    Part 2 literally took like 45 seconds??? What??? Why so easy???

---
+++ solutions/day9part2.py
@@ -0,0 +1,34 @@
+with open("Advent-of-Code-2023/solutions/files/day9input.txt", "r") as file:
+    fileLines = file.readlines()
+
+total = 0
+
+for line in fileLines:
+    sequenceArray = []
+
+    currentLineArray = line.split()
+    currentLineArray = [int(value) for value in currentLineArray]
+
+    sequenceArray.append(currentLineArray)
+
+    allZeroes = False
+    while not allZeroes:
+        numArray = sequenceArray[0]
+        intermediateArray = []
+
+        for i in range(len(numArray) - 1):
+            dif = numArray[i+1] - numArray[i]
+            intermediateArray.append(dif)
+        
+        sequenceArray.insert(0, intermediateArray)
+        
+        arrayCheck = [value for value in intermediateArray if value != 0]
+        if len(arrayCheck) == 0:
+            allZeroes = True
+
+    for i in range(len(sequenceArray) - 1):
+        sequenceArray[i+1].insert(0, sequenceArray[i+1][0] - sequenceArray[i][0])
+    
+    total += sequenceArray[-1][0]
+
+print(total)
