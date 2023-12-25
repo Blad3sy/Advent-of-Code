4b26f1148abc660eaa566a9ed43132d2b1d97ed4
Bladesy <71397567+Blad3sy@users.noreply.github.com>
2023-12-25 18:41:31 +0000

    Completed Day 25 and AOC 2023
    And with that, i'm done!
    Managed to complete this last day but it wasn't the most honest of ways to do it - ah well, it was fun!
    
    Thanks to whoever suggested using Networkx (I cannot remember who).
    
    This was wonderful! I will be doing AOC 2024!

---
+++ 2023/solutions/day25.py
@@ -0,0 +1,40 @@
+import networkx as nx
+
+'''class Graph():
+    def __init__(self):
+        self.nodes = {}
+    
+    def addNode(self, node):
+        self.nodes[node] = []
+    
+    def addConnection(self, node1, node2):
+        self.nodes[node1].append(node2)
+        self.nodes[node2].append(node1)
+    
+    def getAllNodes(self):
+        return list(self.nodes.keys()) 
+    
+    def getNodeConnections(self, node):
+        return self.nodes[node]
+
+    def displayNodes(self):
+        for node in self.nodes:
+            print(node, "->", self.nodes[node])'''
+    
+with open("Advent-of-Code/2023/files/day25input.txt", "r") as file:
+    fileLines = file.readlines()
+
+maingraph = nx.Graph()
+
+for line in fileLines:
+    thisID = line.strip().split(":")[0]
+    connections = line.strip().split(":")[1].strip().split()
+
+    for connection in connections:
+        maingraph.add_edge(thisID, connection, capacity = 100)
+
+outputs = nx.minimum_cut(maingraph, "hgg", "jlx")
+print(len(outputs[1][0]) * len(outputs[1][1]))
+
+# its a little cheaty, I don't care! I had to figure out how to use this library and it was not easy.
+# I read many articles.
